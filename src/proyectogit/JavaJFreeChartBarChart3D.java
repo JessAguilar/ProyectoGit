6f77be249bfb792a2f1e3f36240b9d7b4a55221b
xxjuanandresxx <j.andrescastillo.711@gmail.com>
2018-08-31 17:54:55 -0400

    Nada3

--- src/proyectogit/JavaJFreeChartBarChart3D.java
+++ src/proyectogit/JavaJFreeChartBarChart3D.java
@@ -18,32 +18,110 @@ import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.ui.ApplicationFrame;
 import org.jfree.ui.RefineryUtilities;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import proyectogit.conexion;
 
 /**
  *
  * @author Juan Andres
  */
 public class JavaJFreeChartBarChart3D extends ApplicationFrame{
-    public JavaJFreeChartBarChart3D(final String title) {
-
-		super(title);
-
-		final CategoryDataset dataset = createDataset();
-		final JFreeChart chart = createChart(dataset);
-		final ChartPanel chartPanel = new ChartPanel(chart);
-		chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
-		setContentPane(chartPanel);
-
-	}
-
+    
+    private static conexion conexion;
+    private Connection con;
+    
+    public JavaJFreeChartBarChart3D(final String title,boolean control) {
+        super(title);
+        if(control == true){
+            final CategoryDataset dataset = createDataset();
+            final JFreeChart chart = createChart(dataset);
+            final ChartPanel chartPanel = new ChartPanel(chart);
+            chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
+            setContentPane(chartPanel);
+        }
+        else{
+            final CategoryDataset dataset2 = createDataset2();
+            final JFreeChart chart2 = createChart(dataset2);
+            final ChartPanel chartPanel2 = new ChartPanel(chart2);
+            chartPanel2.setPreferredSize(new java.awt.Dimension(500, 270));
+            setContentPane(chartPanel2);
+        }
+		
+    }
+    
 	public CategoryDataset createDataset() {
-
+                
 		final DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-		dataset.addValue(25.0, "Series 1", "Category 1");
+                try {
+                    String sql = "SELECT\n" +
+                                "	IDPersonaSalida,\n" +
+                                "	SUM(llamada.costoTotal)\n" +
+                                "FROM\n" +
+                                "	llamada \n" +
+                                "GROUP BY IDPersonaSalida ";
+                    con = conexion.getInstance().getConnection();
+                    Statement st = con.createStatement();
+                    ResultSet rs = st.executeQuery(sql);
+                    while(rs.next())
+                    {   
+                      
+                            dataset.addValue(rs.getDouble(2),"ID:"+Integer.toString(rs.getInt(1)), "Gastos de Llamada");
+                        
+                    }
+                    con.commit();
+                    con.close();
+
+                } 
+                catch (SQLException ex) {
+                    Logger.getLogger(Modelo.class.getName()).log(Level.SEVERE, null, ex);
+                }
 		return dataset;
 
 	}
 
+        public CategoryDataset createDataset2() {
+                
+		final DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+                double cLocal=0,cInternacional=0,cCelular=0;
+                String comparative;
+                try {
+                    String sql2 = "SELECT llamada.tipoLlamada\n" +
+                    "FROM llamada";
+                    con = conexion.getInstance().getConnection();
+                    Statement st2 = con.createStatement();
+                    ResultSet rs2 = st2.executeQuery(sql2);
+                    while(rs2.next())
+                    {   
+                        comparative = rs2.getString(1);
+                        if(comparative.equals("Local")){
+                            cLocal++;  
+                        }
+                        if(comparative.equals("Internacional")){
+                            cInternacional++;
+                        }
+                        if(comparative.equals("Celular")){
+                            cCelular++;
+                        }
+                    }
+                    dataset.addValue(cLocal,"Local", "Tipos de Llamada");
+                    dataset.addValue(cInternacional,"Internacional", "Tipos de Llamada");
+                    dataset.addValue(cCelular,"Celular", "Tipos de Llamada");
+                    con.commit();
+                    con.close();
+
+                } 
+                catch (SQLException ex) {
+                    Logger.getLogger(Modelo.class.getName()).log(Level.SEVERE, null, ex);
+                }
+		return dataset;
+
+	}
+        
 	public JFreeChart createChart(final CategoryDataset dataset) {
 
 		final JFreeChart chart = ChartFactory.createBarChart3D("3D Bar Chart Demo", // chart
@@ -69,13 +147,6 @@ public class JavaJFreeChartBarChart3D extends ApplicationFrame{
 		return chart;
 	}
         
-        public static void main(final String[] args) {
 
-		final JavaJFreeChartBarChart3D demo = new JavaJFreeChartBarChart3D("3D Bar Chart Demo 3");
-		demo.pack();
-		RefineryUtilities.centerFrameOnScreen(demo);
-		demo.setVisible(true);
-                
-	}
     
 }
